bb0d95d9618c52a8c8a05b2238e476eeb63541c3
Jeswin <jeswinpk@agilehead.com>
2026-08-26 06:16:12 +0530

    gostdlib: close direct sort profiles

--- gostdlib/src/internal/facets/provider-sort.ts
+++ gostdlib/src/internal/facets/provider-sort.ts
@@ -4,3 +4,8 @@ export {
   Sort as SortCanonical,
   Stable as SortStableCanonical,
 } from "../portable/sort/cooperative.js";
+export {
+  type Interface as SortInterfaceDirect,
+  Sort as SortDirect,
+  Stable as SortStableDirect,
+} from "../portable/sort/order.js";
